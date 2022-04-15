5d5759090fff07be53b78767e7c01e4a67a248ff
Nikhil081200 <nikhilprasad55555@gmail.com>
2022-04-15 22:49:41 +0530

    Sprint

---
+++ ConsoleAppTask/ConsoleAppTask/Models/RegDet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppTask.Models
+{
+    class RegDet
+    {
+        public int Rid { get; set; }
+        public string FirstName { get; set; }
+        public string Mailid { get; set; }
+        public string Contactno { get; set; }
+        public int? Expr { get; set; }
+        public string Skillset { get; set; }
+    }
+}
